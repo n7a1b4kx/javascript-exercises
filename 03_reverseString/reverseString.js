3f3391f6c795efab75a50a4900284ecced640cc7
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-28 16:32:51 +0500

    create: build the boilerplate algorithm

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse = '';
+    let length = word.length;
+    for (let i = 0; i < length; i++) {
+        reverse[1] += word[i];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
