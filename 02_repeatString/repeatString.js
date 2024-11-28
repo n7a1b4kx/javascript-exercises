78fdfcbd2747c80170d8e1c5b2710b70bff23553
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-28 16:01:01 +0500

    fix: add negative integar handle case if else conditionals

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function(word, times) {
     let string = '';
-    for (let i = 0; i < times; i++) {
-        string += word;
+    if (times < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < times; i++) {
+            string += word;
+        }
+        return string;
     }
-    return string;
 };
 
 // Do not edit below this line
