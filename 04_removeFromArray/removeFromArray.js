4c6cf3ba20a08178599e1a7cf712e405ba42c21c
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-29 14:54:49 +0500

    create: add multiple arg support and conditional for elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,28 @@
-const removeFromArray = function(array, element) {
+const removeFromArray = function(array, ...element) // ... three dots are used to capture other arguments and formatted as an array
+{
     let length = array.length;
     let newArray = [];
-    for (let i = 0; i < length; i++) {
-        if (array[i] === element) {
-            continue;
-        } else {
-            newArray.push(array[i]);
+    if (element.length === 1) {
+        for (let i = 0; i < length; i++) {
+            if (array[i] === element[0]) {
+                continue;
+            } else {
+                newArray.push(array[i]);
+            }
+        }
+        return newArray;
+    } else {
+        for (let i = 0; i < length; i++) {
+            for (const el of element) {
+                if (array[i] === el) {
+                    continue;
+                } else {
+                    newArray.push(array[i]);
+                }
+            }
         }
     }
-    return newArray
+    
 };
 
 // Do not edit below this line
