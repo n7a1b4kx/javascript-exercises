2b1af450c8947719f682b703fea64b084bc3871f
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-29 16:36:18 +0500

    create: algo for sum all numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(snum, endnum) {
+    let total = 0;
+    for ( snum; snum <= endnum; snum++) {
+        total += snum;
+    }
+    return total;
 };
 
 // Do not edit below this line
