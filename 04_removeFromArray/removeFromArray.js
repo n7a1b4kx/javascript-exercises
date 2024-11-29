a365764bcbfee57e4d25a87677fccabf2712eda7
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-29 14:18:57 +0500

    create: the algorithm for removing an element
    created an algorithm for removing an element from an array used
    for loop for match of element skip but other than that use push method
    to append element to newArray!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element) {
+    let length = array.length;
+    let newArray = [];
+    for (let i = 0; i < length; i++) {
+        if (array[i] === element) {
+            continue;
+        } else {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
