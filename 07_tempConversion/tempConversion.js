8acdeae5fbc164a856b7b63f584b0b9fdfd2378c
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-30 13:39:41 +0500

    create: add formula for celsius and fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const CELSIUS = (temperature - 32) * 5 / 9;
+  return CELSIUS;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const FAHRENHEIT = temperature * 9 / 5 + 32;
+  return FAHRENHEIT;
 };
+
+console.log(convertToCelsius(100));
 
 // Do not edit below this line
-module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
-};
+// module.exports = {
+//   convertToCelsius,
+//   convertToFahrenheit
+// };
