82670524c53b7b48bdd292282385ace69a64c8df
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-30 13:49:31 +0500

    fix: use the correct method for rounding the floating point

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
 const convertToCelsius = function(temperature) {
   const CELSIUS = (temperature - 32) * 5 / 9;
-  let temp = CELSIUS.toFixed(1);
+  let temp = Math.round(CELSIUS * 10) / 10;
   return temp;
 };
 
 const convertToFahrenheit = function(temperature) {
   const FAHRENHEIT = temperature * 9 / 5 + 32;
-  let temp = FAHRENHEIT.toFixed(1);
+  let temp = Math.round(FAHRENHEIT * 10) / 10;
   return temp;
 };
 
