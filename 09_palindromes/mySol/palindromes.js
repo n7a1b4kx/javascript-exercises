0e3344953ac3d716c9c488bd81cabb542b55fde0
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-13 18:10:47 +0500

    Implement: function for checking phrase
    used for two var for capturing the phrase and then comparing them to get answer

--- 09_palindromes/mySol/palindromes.js
+++ 09_palindromes/mySol/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let length = phrase.length;
+    let forward = '', backward = '';
+    for (let i = 0; i < length; i++) {
+        (phrase[i] !== '.' && phrase[i] !== ',' && phrase[i] !== '!' && phrase[i] !== '?' && phrase[i] !== ' ') ? forward += phrase[i]: console.log(`\t>${phrase[i]}<`);
+    }
+    for (let i = length - 1; i >= 0; i--) {
+        backward += forward[i];
+    }
+    return forward === backward;
 };
 
 // Do not edit below this line
