bdcad77012f36dad61bcf55f734a37b4f863e113
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-14 13:20:26 +0500

    Add: func to retreive the book title

--- 11_getTheTitles/mySol/getTheTitles.js
+++ 11_getTheTitles/mySol/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let title = [];
+    let size = books.length;
+    for (let i = 0; i < size; i++) {
+        let book = books[i]['title'];
+        title.push(book);
+    }
+    return title;
+}
 
-};
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
-module.exports = getTheTitles;
+//module.exports = getTheTitles;
