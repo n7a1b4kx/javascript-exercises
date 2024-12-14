38fb7a01d0bfcc868048841ec90043815436e6d5
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-14 17:23:57 +0500

    remove: testing subject

--- 12_findTheOldest/mySol/findTheOldest.js
+++ 12_findTheOldest/mySol/findTheOldest.js
@@ -9,31 +9,8 @@ const findTheOldest = function(peoples) {
         return people.age = people.yearOfDeath - people.yearOfBirth;
     });
 
-    peoples.forEach((people) => {
-        console.log(people);
-    });
-
-    return currentYear;
+    return peoples;
 }
-
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-    },
-]
-
-findTheOldest(people)
 
 // Do not edit below this line
 module.exports = findTheOldest;
