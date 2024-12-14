a39fce9bd3e37677eb7676394795f7a8dab71d8c
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-14 12:21:07 +0500

    fix: add conditional for negative integers

--- 10_fibonacci/mySol/fibonacci.js
+++ 10_fibonacci/mySol/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = function(number) {
     let fiboArray = [];
+    number = parseInt(number);
+    if (number === 0) {
+        return 0;
+    } else if (number < 0) {
+        return 'OOPS';
+    }
     for (let i = 0; i < number; i++) {
         (i === 0) ? fiboArray.push(1): (i === 1) ? fiboArray.push(1): fiboArray.push(fiboArray[i-1] + fiboArray[i-2]);
     }
