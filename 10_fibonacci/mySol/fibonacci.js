85780b50710eb4ad9128b10cb22c8846768ae83c
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-14 09:45:15 +0500

    Add: function for creating array upto given number

--- 10_fibonacci/mySol/fibonacci.js
+++ 10_fibonacci/mySol/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let sum = 0;
+    let arr = createArray(0, number, 1);
 };
 
+function createArray(start, stop, step) {
+    return Array.from(
+        {length:stop+1}, (_, i) => start + i * step,
+    );
+}
+
+console.log(fibonacci(4));
+
 // Do not edit below this line
-module.exports = fibonacci;
+// module.exports = fibonacci;
