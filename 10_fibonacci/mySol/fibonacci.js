1bf9e7bc49826a887c74171d35a0645d2361a314
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-14 12:13:44 +0500

    fix: give better name & remove comment

--- 10_fibonacci/mySol/fibonacci.js
+++ 10_fibonacci/mySol/fibonacci.js
@@ -1,12 +1,10 @@
 const fibonacci = function(number) {
-    let newArr = [];
+    let fiboArray = [];
     for (let i = 0; i < number; i++) {
-        (i === 0) ? newArr.push(1): (i === 1) ? newArr.push(1): newArr.push(newArr[i-1] + newArr[i-2]);
+        (i === 0) ? fiboArray.push(1): (i === 1) ? fiboArray.push(1): fiboArray.push(fiboArray[i-1] + fiboArray[i-2]);
     }
-    console.log(newArr);
+    return fiboArray.at(-1);
 }
 
-fibonacci(6);
-
 // Do not edit below this line
-// module.exports = fibonacci;
+module.exports = fibonacci;
