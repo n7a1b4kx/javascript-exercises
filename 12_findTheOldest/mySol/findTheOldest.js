554a989b311485972eda31c8c22c71e8e21cda61
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-14 17:02:45 +0500

    Implement: function for getting current year for user

--- 12_findTheOldest/mySol/findTheOldest.js
+++ 12_findTheOldest/mySol/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(peoples) {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    peoples.forEach((people) => {
+        return (people.yearOfDeath === undefined) ? people.yearOfDeath = currentYear: people.yearOfDeath;
+    });
 
-};
+    peoples.forEach((people) => {
+        console.log(people);
+    });
+
+    return currentYear;
+}
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+    },
+]
+
+findTheOldest(people)
 
 // Do not edit below this line
 module.exports = findTheOldest;
